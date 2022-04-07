e69e7156d2649445c3c1f3a14a547b795ccf93c3
Cezary Krawiec <web@cezarykrawiec.pl>
2022-04-07 23:28:38 +0200

    add SecurityController to auth user

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodoController extends AbstractController
 {
-    #[Route('/', name: 'app_todo')]
+    #[Route('/', name: 'app_todo'), IsGranted('ROLE_ADMIN')]
     public function index(): Response
     {
         return $this->render('todo/index.html.twig', [
